c0d9979b6edd08661dc20e77a78b058fb4ce7226
sangmoki <yoochang4176@naver.com>
2024-10-22 17:47:35 +0900

    Feat: "RecvBuffer를 분리하기 위한 클래스 생성"

---
+++ ServerCore/RecvBuffer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public class RecvBuffer
+    {
+        ArraySegment<byte> _buffer;
+        int _readPos;
+        int _writePos;
+
+        public RecvBuffer(int bufferSize)
+        {
+            // 버퍼 초기화
+            _buffer = new ArraySegment<byte>(new byte[bufferSize], 0, bufferSize);
+        }
+
+        public int DataSize { get { return _writePos - _readPos; } }
+        public int FreeSize { get { return _buffer.Count - _writePos; } }
+
+        public ArraySegment<byte> ReadSegment
+        {
+            get { return null; }
+        }
+
+        public ArraySegment<byte> WriteSegment
+        {
+            get { return null; }
+        }
+
+    }
+}
